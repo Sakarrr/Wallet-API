8da540c5b93d268f1199ec909441adf8f2a9ec98
Sakarr <ctha.sakar@gmail.com>
2024-06-29 13:28:33 +0545

    Handling user logins

--- modules/users/controllers/userLogin.js
+++ modules/users/controllers/userLogin.js
@@ -1,6 +1,31 @@
-const userLogin = (req, res) => {
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userLogin = async (req, res) => {
+  const Users = mongoose.model("users");
+  const { email, password } = req.body;
+
+  // Validations
+  try {
+    if (!email || !password) throw "Please fill both inputs";
+
+    const getUser = await Users.findOne({
+      email: email,
+    });
+
+    if (!getUser) throw "Email doesn't exist";
+
+    const matched = await bcrypt.compare(password, getUser.password);
+
+    if (!matched) throw "Email and password doesn't match";
+  } catch (e) {
+    res.status(400).json({ status: "Failed", message: e });
+    return;
+  }
+
+  // Success
   res.status(200).json({
-    status: "User Login",
+    status: "User Logged in Successfully",
   });
 };
 
