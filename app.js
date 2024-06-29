749a88e8afb4e25f03a3bdd73813a6e26d0ef3d9
Sakarr <ctha.sakar@gmail.com>
2024-06-29 20:17:54 +0545

    Handling expenses on wallet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./modules/users/users.routes");
 const incomeRouter = require("./modules/income/income.routes");
+const expenseRouter = require("./modules/expenses/expense.routes");
 
 require("dotenv").config();
 
@@ -24,6 +25,7 @@ mongoose
 // Routes
 app.use("/users", userRouter);
 app.use("/income", incomeRouter);
+app.use("/expense", expenseRouter);
 
 app.listen(8000, () => {
   console.log("Server started successfully!");
---
+++ modules/expenses/controllers/addExpense.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const addExpense = async (req, res) => {
+  const Users = mongoose.model("users");
+
+  const { amount, remarks } = req.body;
+
+  try {
+    if (!amount) throw "Please fill desired amount";
+    if (amount < 1) throw "Amount must be above 1";
+    if (!remarks) throw "Please fill remarks";
+    if (remarks.length < 2) throw "Remarks must be atleast 2 chars long";
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed",
+      message: e,
+    });
+    return;
+  }
+
+  // Success
+  await Users.updateOne(
+    { _id: req.user._id },
+    { $inc: { balance: amount * -1 } },
+    { runValidators: true }
+  );
+  res.status(200).json({
+    status: "Expense was added",
+  });
+};
+
+module.exports = addExpense;
